62657946cef400a1bdff5c91f36f055db1c9df3d
Argym <argymnasthiar@gmail.com>
2023-03-13 20:39:15 +0700

    recover

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import LearnHOC from "./pages/LearnHOC";
-import 
 
 function App() {
   const Button = (props) => <button style={props.style}>Click Me</button>;
-  const Text = (props) => <p style={props.style}>Click Me</p>;
+  const Text = (props) => <p style={props.style}>Hello</p>;
 
   const StyleButton = LearnHOC(Button);
   const StyledText = LearnHOC(Text);
   return (
     <div className="App">
-      {/* <StyleButton />
-      <StyledText /> */}
-      <Routes>
-        <Route path="/" element={<LearnHOC />} />
-      </Routes>
+      <StyleButton />
+      <StyledText />
+
+      <h1>Hello, world!</h1>
+      <p>This is some example text.</p>
     </div>
   );
 }
